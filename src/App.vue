28edc35dcd227846d30bec118d477336c43fd511
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-01-31 17:27:27 +0900

    특정 단어 있는 것만 출력

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,19 @@ import Mainpokemon from "@/assets/pokemon.json"
 
 
 const parentMessage = ref('Parent')
-/*
-const items = ref([
-	{
-	    message : 'Foo'
-	},{
-	    message : 'Bar'}
-])
-*/
 const pokemons = ref(Mainpokemon.pokemon)
 </script>
 
 <template>
-<!--
-<li v-for="(item,index) in items">
-{{parentMessage}} - {{index}} - {{item.message}}
-</li>
--->
-<li v-for="(pokemon,index) in pokemons" :key="index">
+    <ul>
+	 <template v-for="(pokemon,index) in pokemons" :key="index">
+<li v-if="pokemon.type.includes('Electric') ">
 번호 : {{pokemon.num}}<br> 
 이름  : {{pokemon.name}} <br>
 속성 : {{pokemon.type}}<br>   약점 :  {{pokemon.weaknesses}} <br>
 <img :src="pokemon.img" alt="Pokemon Image" style="max-width: 100px; max-height: 100px;"><br>
-
+<hr>
 </li>
+</template>
+</ul>
 </template>
