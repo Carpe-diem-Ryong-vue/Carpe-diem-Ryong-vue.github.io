e8603c383808afd42046f554d3b77de6d7cab771
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-02-01 10:31:25 +0900

    alert 예제

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
-import {reactive, computed, ref } from 'vue'
+function say(message) {
+    alert(message)
+}
 
-const author = reactive({
-    name: 'John Doe',
-    books: [1,2,3]
-    })
-// a computed ref
-const publishedBooksMessage = computed( () => {return author.books.length > 2 ? 'Yes' : 'No' })
 </script>
 
 <template>
-    <span> 😎 :  </span>
-    <span> {{author.books.length > 2 ? 'Yes' : 'No'}} </span>
-    <div>computed ⬇️</div>
-    <div>{{publishedBooksMessage}}</div>
+    <button @click="say('hello')">Say hello</button>
+    <button @click="say('bye')">Say bye</button>
+
 </template>
