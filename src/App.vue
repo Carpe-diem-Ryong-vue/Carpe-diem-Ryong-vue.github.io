7552bb8a8d49af695fdf23b05eeed4183ed59174
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-02-01 17:35:48 +0900

    자식컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <script setup>
-import { ref } from 'vue'
-let id = 0
-const newTodo = ref('')
-const todos = ref([
-	{ id : id++, issue: 'BackLog'},{id : id++, issue: 'Todo'},
-	{ id : id++, issue: 'Going Hawaii'}
-])
 
-function addTodo(e){
-    e.preventDefault();  // 이벤트 막기
-    todos.value.push({id : id++,issue : newTodo.value})
-    newTodo.value=''
-}
+import Child from './components/Child.vue'
 
-function removeTodo(todo){
-    console.log('dddd')
-    todos.value = todos.value.filter(item => item !== todo)
-}
 </script>
 <template>
-    <form @submit="addTodo">  <!-- 위에 e.preventDefault() 대신에  submit.prevnet 를 달아도 됨. -->
-	<input v-model="newTodo">
-	<button>할 일 추가</button>
-    </form>
-    <ul>
-	<li v-for="todo in todos">
-	    {{ todo.issue }} - <button @click="removeTodo(todo)">Done</button>
-	</li>
-    </ul>
+<Child/>
+
 </template>
