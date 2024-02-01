8b025bd1e1a95c37d47d0518baa74067bd3f85da
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-02-01 11:43:55 +0900

    Lifecycle3

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
 import { ref, onUpdated,onMounted,onBeforeUpdate } from 'vue'
 
 const count = ref(0)
+
+onMounted(() => {
+  console.log('Component is mounted')
+})
+
+onBeforeUpdate(() => {
+  console.log('Before update:', document.getElementById('count').textContent)
+})
+    
 onUpdated( () => {
 	console.log(document.getElementById('count').textContent)
 	})
