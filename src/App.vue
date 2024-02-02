c40a1bccde63ce46978ed4f4bb60f5d68f06327f
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-02-02 11:04:59 +0900

    morning exercise

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <script setup>
 import {ref} from 'vue'
 
-const show = ref(ture)
+const show = ref(true)
 const list = ref([1,2,3])
 
 function addList(){
-    list.value.push
+    list.value.push(list.value.length+1)
+}
+function removeList(){
+    list.value.pop()
+}
 
+function reverseList(){
+    list.value.reverse()
+}
+function renderingList(){
+     show.value = !show.value
+}
     
 </script>
 
 <template>
-    <button @click="<?>">List 렌더링 ON/OFF </button>
-    <button @click="<?>">List 추가 </button>
-    <button @click="<?>">List 제거 </button>
-    <button @click="<?>">List 뒤집기 </button>
+    <button @click="renderingList">List 렌더링 ON/OFF </button>
+    <button @click="addList">List 추가 </button>
+    <button @click="removeList">List 제거 </button>
+    <button @click="reverseList">List 뒤집기 </button>
 
-<ul v-if="<?>">
-    <li v-for = "item of List">{{item}}</li>
+<ul v-if="show">
+    <li v-for = "item of list" :key="itme">{{item}}</li>
 </ul>
 <p v-else-if="list.length">List is not empty, but hidden. </p>
 <p v-else>List is empty.</p>
